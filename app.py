21759a43d3712a2dbb2d4264dc83a27c9b602829
Jerome Schmidt <schmidt.jerome@yahoo.com>
2019-10-16 12:47:35 -0700

    initial commit with app.py started and templates and partials folders started

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request, redirect, url_for
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+import os
+from datetime import datetime
+
+host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/brokerage_users')
+client = MongoClient(host=f'{host}?retryWrites=false')
+db = client.get_default_database()
+users = db.brokerage_users
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    """Show log in screen."""
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
