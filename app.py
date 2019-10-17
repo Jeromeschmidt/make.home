e43ddf47262e80d354c81708152b2dc0ba80d8f6
Jerome Schmidt <schmidt.jerome@yahoo.com>
2019-10-17 15:21:44 -0700

    fixing heroku push

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 import os
 from datetime import datetime
 import random
+from flask import Flask, render_template, request, redirect, url_for
 
 host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/MS_staff')
 client = MongoClient(host=f'{host}?retryWrites=false')
